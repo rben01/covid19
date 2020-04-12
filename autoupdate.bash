7097e5c90d932c4d66580facc16306afc6635d3c
Robert Bennett <rltbennett@icloud.com>
2020-04-12 00:02:46 -0400

    Made autoupdate abort on error (bash &&); moved stylesheet into readmREADME.asciidoc

--- autoupdate.bash
+++ autoupdate.bash
@@ -1,7 +1,8 @@
-curr_branch="$(git rev-parse --abbrev-ref HEAD)"
-git checkout gh-pages
-python src/case_tracker.py --use-web-data --create-data-table
-git add -A .
-git commit -m "Auto update with new data"
-git push
-git checkout "$curr_branch"
+curr_branch="$(git rev-parse --abbrev-ref HEAD)" &&
+	git checkout gh-pages &&
+	python src/case_tracker.py --use-web-data --create-data-table &&
+	asciidoctor -b html5 -o index.html README.asciidoc &&
+	git add -A . &&
+	git commit -m "Auto update with new data" &&
+	git push &&
+	git checkout "$curr_branch"
