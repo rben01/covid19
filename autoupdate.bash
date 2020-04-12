e678e30753f5984791f716422ea84eabaeb92693
Robert Bennett <rltbennett@icloud.com>
2020-04-11 23:57:47 -0400

    Auto update with new data

--- autoupdate.bash
+++ autoupdate.bash
@@ -1,7 +1,9 @@
 curr_branch="$(git rev-parse --abbrev-ref HEAD)"
+echo "On branch $curr_branch"
 git checkout gh-pages
 python src/case_tracker.py --use-web-data --create-data-table
 git add -A .
 git commit -m "Auto update with new data"
 git push
 git checkout "$curr_branch"
+echo "On branch $curr_branch"
