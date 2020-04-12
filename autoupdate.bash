43542ea90cf21436908ee66afe5e93ee2c6f3307
Robert Bennett <rltbennett@icloud.com>
2020-04-12 00:30:18 -0400

    Migrating to master/docs github pages

--- autoupdate.bash
+++ autoupdate.bash
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
+ghpages_branch='master'
 
 curr_branch="$(git rev-parse --abbrev-ref HEAD)" &&
-	git checkout gh-pages &&
+	git checkout "$ghpages_branch" &&
 	python src/case_tracker.py --use-web-data --create-data-table &&
-	asciidoctor -b html5 -o index.html README.asciidoc &&
+	asciidoctor -b html5 -o docs/index.html README.asciidoc &&
 	git add -A . &&
 	git commit -m "Auto update with new data" &&
 	git push &&
 	git checkout "$curr_branch" &&
-	if [ "$curr_branch" != "gh-pages" ]; then echo "Back on branch '$curr_branch'"; else echo "Still on branch 'gh-pages'"; fi
+	if [ "$curr_branch" != "$ghpages_branch" ]; then echo "Back on branch '$curr_branch'"; else echo "Still on branch '$ghpages_branch'"; fi
