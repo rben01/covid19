dab16bb55b6b1f449ef70578735d257d2cf47bbe
Robert Bennett <rltbennett@icloud.com>
2020-04-26 22:55:43 -0400

    Auto update with new data

--- autoupdate.bash
+++ autoupdate.bash
@@ -3,7 +3,7 @@ ghpages_branch='master'
 
 curr_branch="$(git rev-parse --abbrev-ref HEAD)" &&
 	git checkout "$ghpages_branch" &&
-	python src/case_tracker.py --use-web-data --create-data-table --force-graphs --force-interactive &&
+	python src/case_tracker.py --use-web-data --create-data-table --force-graphs --force-interactive --no-timeline &&
 	touch README.asciidoc &&
 	asciidoctor --base-dir docs -o index.html README.asciidoc &&
 	git status &&
