ed5ea3955e5dad5b687adfa757d56e1f3a5b3004
Robert Bennett <rltbennett@icloud.com>
2020-06-11 22:20:46 -0400

    Changed data format again

--- src/data_to_json.py
+++ src/data_to_json.py
@@ -23,14 +23,20 @@ def nan_to_none(x):
 def data_to_json(outfile: Path):
     df: pd.DataFrame = pd.read_csv(Paths.DATA_TABLE)
 
-    usa_df = (
+    is_state = df[Columns.STATE].notna()
+    usa_df = df[is_state].drop(columns=Columns.COUNTRY)
+    countries_df = df[~is_state].drop(
+        columns=[Columns.STATE, Columns.TWO_LETTER_STATE_CODE]
+    )
+
+    usa_geo_df = (
         get_usa_states_geo_df()[GEO_COL_REMAPPER.keys()]
         .drop_duplicates(REGION_NAME_COL)
         .rename(columns=GEO_COL_REMAPPER)
         .set_index("region_name")
         .sort_index()
     )
-    countries_df = (
+    countries_geo_df = (
         get_countries_geo_df()[GEO_COL_REMAPPER.keys()]
         .drop_duplicates(REGION_NAME_COL)
         .rename(columns=GEO_COL_REMAPPER)
@@ -38,36 +44,47 @@ def data_to_json(outfile: Path):
         .sort_index()
     )
 
-    category_map = {}
-    cat_cols = [
-        Columns.STATE,
-        Columns.COUNTRY,
-        Columns.TWO_LETTER_STATE_CODE,
-        Columns.DATE,
-    ]
+    # category_map = {}
+    # cat_cols = [
+    #     Columns.STATE,
+    #     Columns.COUNTRY,
+    #     Columns.TWO_LETTER_STATE_CODE,
+    #     Columns.DATE,
+    # ]
 
-    records = {}
-    for col in df.columns:
-        if col in cat_cols and False:
-            col_as_cat = df[col].astype("category")
-            category_map[col] = dict(
-                zip(col_as_cat.cat.codes, df[col].map(nan_to_none),)
-            )
-            records[col] = col_as_cat.cat.codes.tolist()
-        else:
-            records[col.lower().replace(" ", "_").replace("cap.", "capita")] = list(
+    data = {"usa": {}, "world": {}}
+    for name, df in [("usa", usa_df), ("world", countries_df)]:
+        r = data[name]
+        if "records" not in r:
+            r["records"] = {}
+
+        d = r["records"]
+
+        for col in df.columns:
+            # if col in cat_cols and False:
+            #     col_as_cat = df[col].astype("category")
+            #     category_map[col] = dict(
+            #         zip(col_as_cat.cat.codes, df[col].map(nan_to_none),)
+            #     )
+            #     d[col] = col_as_cat.cat.codes.tolist()
+            # else:
+            d[col.lower().replace(" ", "_").replace("cap.", "capita")] = list(
                 map(nan_to_none, df[col].tolist())
             )
 
-    data = {
-        "records": records,
-        "geo": {
-            "usa": usa_df.to_dict(orient="index"),
-            "world": countries_df.to_dict(orient="index"),
-        },
-    }
-    if category_map:
-        data["categories"] = category_map
+    for name, df in [("usa", usa_geo_df), ("world", countries_geo_df)]:
+        r = data[name]
+        r["geo"] = df.to_dict(orient="index")
+
+    # data = {
+    #     "records": records,
+    #     "geo": {
+    #         "usa": usa_geo_df.to_dict(orient="index"),
+    #         "world": countries_geo_df.to_dict(orient="index"),
+    #     },
+    # }
+    # if category_map:
+    #     data["categories"] = category_map
 
     if outfile is not None:
         with outfile.open("w") as f:
