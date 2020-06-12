dfb8f997edbaf47558e3f37ee1da92727aa7a517
Robert Bennett <rltbennett@icloud.com>
2020-06-11 21:16:57 -0400

    Beginning migration to js-based plots

---
+++ src/data_to_json.py
@@ -0,0 +1,45 @@
+# %%
+import json
+from pathlib import Path
+
+import pandas as pd
+
+from constants import Columns, Paths
+
+
+def nan_to_none(x):
+    return None if pd.isna(x) else x
+
+
+def data_to_json(outfile: Path):
+    df: pd.DataFrame = pd.read_csv(Paths.DATA_TABLE)
+
+    category_map = {}
+    cat_cols = [
+        Columns.STATE,
+        Columns.COUNTRY,
+        Columns.TWO_LETTER_STATE_CODE,
+        Columns.DATE,
+    ]
+
+    records = {}
+    for col in df.columns:
+        if col in cat_cols:
+            col_as_cat = df[col].astype("category")
+            category_map[col] = dict(
+                zip(col_as_cat.cat.codes, df[col].map(nan_to_none),)
+            )
+            records[col] = col_as_cat.cat.codes.tolist()
+        else:
+            records[col] = list(map(nan_to_none, df[col].tolist()))
+
+    data = {"categories": category_map, "records": records}
+    if outfile is not None:
+        with outfile.open("w") as f:
+            json.dump(data, f, indent=2)
+
+    return data
+
+
+data_to_json(outfile=Paths.DATA / "data.json")
+None
