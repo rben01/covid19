63e348fc40ab49611e9c422cc32dd49914514614
Robert Bennett <rltbennett@icloud.com>
2020-06-12 00:43:24 -0400

    Actually got basic plots working, yay

--- docs/html/plots.ts
+++ docs/html/plots.ts
@@ -33,14 +33,14 @@ function plotData(covidData: Data, geoData: GeoJson, date: DateString) {
 	const data = {
 		cases: [],
 		dates: [],
-		location: [],
+		locations: [],
 		text: [],
 	};
 	covidData.date.forEach((d, i) => {
 		if (d === date) {
 			data.cases.push(covidData.cases[i]);
 			data.dates.push(d);
-			data.location.push(covidData.codes[i]);
+			data.locations.push(covidData.codes[i]);
 			data.text.push(covidData.names[i]);
 		}
 	});
@@ -49,14 +49,21 @@ function plotData(covidData: Data, geoData: GeoJson, date: DateString) {
 	const plotData = [
 		{
 			type: "choropleth",
-			geojson: geoData,
-			featureidkey: "properties.ADM0_A3",
+			// geojson: geoData,
+			locationmode: "USA-states",
+			locations: data.locations,
 			z: data.cases,
 			text: data.text,
 		},
 	];
 
-	Plotly.react("usa-1", plotData);
+	const plotLayout = {
+		geo: {
+			scope: "usa",
+		},
+	};
+
+	Plotly.react("usa-1", plotData, plotLayout);
 }
 
 const nowMS = new Date().getTime();
@@ -71,7 +78,58 @@ Promise.all([
 		"https://raw.githubusercontent.com/rben01/covid19/js-migrate/docs/data/geo_world.json",
 	),
 	,
-]).then(([covidData, geoUsa, geoWorld]) => {
-	console.log(covidData.world);
-	plotData(covidData.world, geoWorld, "2020-05-11");
+]).then(([covidData, geoUSA, geoWorld]) => {
+	plotData(covidData.usa, geoUSA, "2020-05-11");
 });
+
+Plotly.d3.csv(
+	"https://raw.githubusercontent.com/plotly/datasets/master/2011_us_ag_exports.csv",
+	function (err, rows) {
+		function unpack(rows, key) {
+			return rows.map(function (row) {
+				return row[key];
+			});
+		}
+
+		var data = [
+			{
+				type: "choropleth",
+				locationmode: "USA-states",
+				locations: unpack(rows, "code"),
+				z: unpack(rows, "total exports"),
+				text: unpack(rows, "state"),
+				zmin: 0,
+				zmax: 17000,
+				colorscale: [
+					[0, "rgb(242,240,247)"],
+					[0.2, "rgb(218,218,235)"],
+					[0.4, "rgb(188,189,220)"],
+					[0.6, "rgb(158,154,200)"],
+					[0.8, "rgb(117,107,177)"],
+					[1, "rgb(84,39,143)"],
+				],
+				colorbar: {
+					title: "Millions USD",
+					thickness: 0.2,
+				},
+				marker: {
+					line: {
+						color: "rgb(255,255,255)",
+						width: 2,
+					},
+				},
+			},
+		];
+
+		var layout = {
+			title: "2011 US Agriculture Exports by State",
+			geo: {
+				scope: "usa",
+				showlakes: true,
+				lakecolor: "rgb(255,255,255)",
+			},
+		};
+
+		Plotly.newPlot("usa-2", data, layout, { showLink: false });
+	},
+);
