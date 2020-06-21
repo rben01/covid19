b11bdb6c200a9bdecab4c1326b2c1bd3ccdee881
Robert Bennett <rltbennett@icloud.com>
2020-06-20 23:18:15 -0400

    Drew the graph background

--- docs/html/built/line_charts.js
+++ docs/html/built/line_charts.js
@@ -1,12 +1,15 @@
-import { dateStrParser } from "./utils.js";
+import { dateStrParser, getFormatter } from "./utils.js";
 const plotAesthetics = (() => {
+    const width = 600, height = 600;
     const pa = {
-        width: 600,
-        height: 600,
+        width: width,
+        height: height,
         axis: {
             margins: {
+                top: 3,
                 bottom: 60,
-                left: 30,
+                left: 40,
+                right: 3,
             },
             width: null,
             height: null,
@@ -14,7 +17,9 @@ const plotAesthetics = (() => {
                 strokeWidth: 1,
                 tickLength: 6,
                 axisColor: "black",
-                gridlineColor: "#888",
+                gridlineColor: "#e4e4e4",
+                labelTranslateX: (7 / 600) * width,
+                labelTranslateY: (7 / 600) * height,
             },
         },
         colors: {
@@ -34,7 +39,7 @@ const dateFormatter = d3.timeFormat("%b %-d");
 export function initializeLineGraph(allCovidData, geoCovidData) {
     lineGraph.datum({ allCovidData, geoCovidData });
     const location = "usa";
-    const count = "dodd";
+    const count = "net";
     const caseType = "cases";
     const svg = lineGraph
         .append("svg")
@@ -42,41 +47,75 @@ export function initializeLineGraph(allCovidData, geoCovidData) {
         .attr("height", plotAesthetics.height);
     const chartArea = svg.append("g").classed("line-chart-area", true);
     const { min_nonzero: _minDateStr, max: _maxDateStr } = allCovidData[location].agg.net.date;
-    const { min_nonzero: minVal, max: maxVal } = allCovidData[location].agg.net[caseType];
+    const { min_nonzero: minVal, max: maxVal } = allCovidData[location].agg[count][caseType];
+    console.log(minVal, maxVal);
     const [minDate, maxDate] = [_minDateStr, _maxDateStr].map(dateStrParser);
     const xScale = d3
         .scaleTime()
         .domain([minDate, maxDate])
-        .range([plotAesthetics.axis.margins.left, plotAesthetics.width]);
+        .range([
+        plotAesthetics.axis.margins.left,
+        plotAesthetics.width - plotAesthetics.axis.margins.right,
+    ]);
     const yScale = d3
         .scaleLog()
         .domain([minVal, maxVal])
-        .range([plotAesthetics.axis.height, 0]);
+        .range([plotAesthetics.axis.height, plotAesthetics.axis.margins.top]);
     const { strokeWidth, axisColor, tickLength, gridlineColor, } = plotAesthetics.axis.style;
     const xTicks = xScale.ticks(d3.timeDay.every(7));
+    const yFormatter = getFormatter(count, caseType, 1);
+    const yTicks = yScale.ticks(15);
     const xAxis = chartArea.append("g").classed("line-chart-x-axis", true);
-    xAxis
-        .append("line")
-        .attr("x1", xScale(minDate))
-        .attr("x2", xScale(maxDate))
-        .attr("y1", yScale(minVal))
-        .attr("y2", yScale(minVal))
-        .attr("stroke", axisColor)
-        .attr("stroke-width", strokeWidth);
+    const yAxis = chartArea.append("g").classed("line-chart-y-axis", true);
     xAxis
         .selectAll()
         .data(xTicks)
         .join("line")
+        .classed("x-axis-tick", true)
         .attr("x1", xScale)
         .attr("x2", xScale)
         .attr("y1", yScale(minVal))
         .attr("y2", yScale(minVal) + tickLength)
         .attr("stroke", axisColor)
         .attr("stroke-width", strokeWidth);
+    yAxis
+        .selectAll()
+        .data(yTicks)
+        .join("line")
+        .classed("y-axis-tick", true)
+        .attr("x1", xScale(minDate))
+        .attr("x2", xScale(minDate) - plotAesthetics.axis.style.tickLength)
+        .attr("y1", yScale)
+        .attr("y2", yScale)
+        .attr("stroke", axisColor)
+        .attr("stroke-width", strokeWidth);
+    xAxis
+        .selectAll()
+        .data(xTicks)
+        .join("line")
+        .classed("x-axis-gridline", true)
+        .attr("x1", xScale)
+        .attr("x2", xScale)
+        .attr("y1", yScale(minVal))
+        .attr("y2", yScale(maxVal))
+        .attr("stroke", plotAesthetics.axis.style.gridlineColor)
+        .attr("stroke-width", plotAesthetics.axis.style.strokeWidth);
+    yAxis
+        .selectAll()
+        .data(yTicks)
+        .join("line")
+        .classed("y-axis-gridline", true)
+        .attr("x1", xScale(minDate))
+        .attr("x2", xScale(maxDate))
+        .attr("y1", yScale)
+        .attr("y2", yScale)
+        .attr("stroke", plotAesthetics.axis.style.gridlineColor)
+        .attr("stroke-width", plotAesthetics.axis.style.strokeWidth);
     xAxis
         .selectAll()
         .data(xTicks)
         .join("text")
+        .classed("x-axis-label", true)
         .text((date) => {
         const dayOfMonth = date.getDate();
         if (dayOfMonth % 7 == 1 && dayOfMonth < 28) {
@@ -86,5 +125,46 @@ export function initializeLineGraph(allCovidData, geoCovidData) {
     })
         .attr("text-anchor", "end")
         .attr("font-size", "70%")
-        .attr("transform", (d) => `translate(${xScale(d) + 9},${yScale(minVal) + tickLength + 7}) rotate(-60)`);
+        .attr("transform", (d) => `translate(${xScale(d) + plotAesthetics.axis.style.labelTranslateX},${yScale(minVal) +
+        tickLength +
+        plotAesthetics.axis.style.labelTranslateY}) rotate(-60)`);
+    yAxis
+        .selectAll()
+        .data(yTicks)
+        .join("text")
+        .classed("y-axis-label", true)
+        .text((y) => {
+        const firstDigit = +y.toString()[0];
+        if (firstDigit <= 4 || firstDigit === 6) {
+            return yFormatter(y);
+        }
+        return "";
+    })
+        .attr("x", xScale(minDate) - plotAesthetics.axis.style.tickLength - 3)
+        .attr("y", yScale)
+        .attr("text-anchor", "end")
+        .attr("dominant-baseline", "middle")
+        .attr("font-size", "70%");
+    xAxis
+        .selectAll()
+        .data([minDate, maxDate])
+        .join("line")
+        .classed("x-axis-axis", true)
+        .attr("x1", xScale)
+        .attr("x2", xScale)
+        .attr("y1", yScale(minVal))
+        .attr("y2", yScale(maxVal))
+        .attr("stroke", axisColor)
+        .attr("stroke-width", strokeWidth);
+    yAxis
+        .selectAll()
+        .data([minVal, maxVal])
+        .join("line")
+        .classed("y-axis-axis", true)
+        .attr("x1", xScale(minDate))
+        .attr("x2", xScale(maxDate))
+        .attr("y1", yScale)
+        .attr("y2", yScale)
+        .attr("stroke", axisColor)
+        .attr("stroke-width", strokeWidth);
 }
--- docs/html/line_charts.ts
+++ docs/html/line_charts.ts
@@ -20,13 +20,17 @@ import { dateStrParser, getFormatter } from "./utils.js";
 type StartFrom = "first_date" | "outbreak";
 
 const plotAesthetics = (() => {
+	const width = 600,
+		height = 600;
 	const pa = {
-		width: 600,
-		height: 600,
+		width: width,
+		height: height,
 		axis: {
 			margins: {
+				top: 3,
 				bottom: 60,
-				left: 30,
+				left: 40,
+				right: 3,
 			},
 			width: (null as unknown) as number,
 			height: (null as unknown) as number,
@@ -34,7 +38,9 @@ const plotAesthetics = (() => {
 				strokeWidth: 1,
 				tickLength: 6,
 				axisColor: "black",
-				gridlineColor: "#888",
+				gridlineColor: "#e4e4e4",
+				labelTranslateX: (7 / 600) * width,
+				labelTranslateY: (7 / 600) * height,
 			},
 		},
 		colors: {
@@ -70,32 +76,8 @@ export function initializeLineGraph(
 	lineGraph.datum({ allCovidData, geoCovidData });
 
 	const location: WorldLocation = "usa";
-	const count: CountMethod = "dodd";
+	const count: CountMethod = "net";
 	const caseType: CaseType = "cases";
-
-	// const xAxis = d3
-	// 	.axisBottom(xScale)
-	// 	.tickValues(xTicks)
-	// 	.tickFormat((date: Date) => {
-	// 		const dayOfMonth = date.getDate();
-	// 		if (dayOfMonth % 7 == 1 && dayOfMonth < 28) {
-	// 			return dateFormatter(date);
-	// 		}
-	// 		return "";
-	// 	});
-
-	// const yFormatter = getFormatter(count, caseType, 1);
-	// const yAxis = d3
-	// 	.axisLeft(yScale)
-	// 	.ticks(6)
-	// 	.tickFormat((n: number) => {
-	// 		const firstDigit = +`${n}`[0];
-
-	// 		if (firstDigit <= 4 || firstDigit % 2 === 0) {
-	// 			return yFormatter(n);
-	// 		}
-	// 		return "";
-	// 	});
 
 	const svg = lineGraph
 		.append("svg")
@@ -106,7 +88,7 @@ export function initializeLineGraph(
 	const { min_nonzero: _minDateStr, max: _maxDateStr } = allCovidData[
 		location
 	].agg.net.date;
-	const { min_nonzero: minVal, max: maxVal } = allCovidData[location].agg.net[
+	const { min_nonzero: minVal, max: maxVal } = allCovidData[location].agg[count][
 		caseType
 	];
 
@@ -115,11 +97,14 @@ export function initializeLineGraph(
 	const xScale = d3
 		.scaleTime()
 		.domain([minDate, maxDate])
-		.range([plotAesthetics.axis.margins.left, plotAesthetics.width]);
+		.range([
+			plotAesthetics.axis.margins.left,
+			plotAesthetics.width - plotAesthetics.axis.margins.right,
+		]);
 	const yScale = d3
 		.scaleLog()
 		.domain([minVal, maxVal])
-		.range([plotAesthetics.axis.height, 0]);
+		.range([plotAesthetics.axis.height, plotAesthetics.axis.margins.top]);
 
 	const {
 		strokeWidth,
@@ -129,29 +114,66 @@ export function initializeLineGraph(
 	} = plotAesthetics.axis.style;
 
 	const xTicks = xScale.ticks(d3.timeDay.every(7));
+	const yFormatter = getFormatter(count, caseType, 1);
+	const yTicks = yScale.ticks(15);
+
 	const xAxis = chartArea.append("g").classed("line-chart-x-axis", true);
-	xAxis
-		.append("line")
-		.attr("x1", xScale(minDate))
-		.attr("x2", xScale(maxDate))
-		.attr("y1", yScale(minVal))
-		.attr("y2", yScale(minVal))
-		.attr("stroke", axisColor)
-		.attr("stroke-width", strokeWidth);
+	const yAxis = chartArea.append("g").classed("line-chart-y-axis", true);
+
+	// Tick marks
 	xAxis
 		.selectAll()
 		.data(xTicks)
 		.join("line")
+		.classed("x-axis-tick", true)
 		.attr("x1", xScale)
 		.attr("x2", xScale)
 		.attr("y1", yScale(minVal))
 		.attr("y2", yScale(minVal) + tickLength)
 		.attr("stroke", axisColor)
 		.attr("stroke-width", strokeWidth);
+	yAxis
+		.selectAll()
+		.data(yTicks)
+		.join("line")
+		.classed("y-axis-tick", true)
+		.attr("x1", xScale(minDate))
+		.attr("x2", xScale(minDate) - plotAesthetics.axis.style.tickLength)
+		.attr("y1", yScale)
+		.attr("y2", yScale)
+		.attr("stroke", axisColor)
+		.attr("stroke-width", strokeWidth);
+
+	// Gridlines
+	xAxis
+		.selectAll()
+		.data(xTicks)
+		.join("line")
+		.classed("x-axis-gridline", true)
+		.attr("x1", xScale)
+		.attr("x2", xScale)
+		.attr("y1", yScale(minVal))
+		.attr("y2", yScale(maxVal))
+		.attr("stroke", plotAesthetics.axis.style.gridlineColor)
+		.attr("stroke-width", plotAesthetics.axis.style.strokeWidth);
+	yAxis
+		.selectAll()
+		.data(yTicks)
+		.join("line")
+		.classed("y-axis-gridline", true)
+		.attr("x1", xScale(minDate))
+		.attr("x2", xScale(maxDate))
+		.attr("y1", yScale)
+		.attr("y2", yScale)
+		.attr("stroke", plotAesthetics.axis.style.gridlineColor)
+		.attr("stroke-width", plotAesthetics.axis.style.strokeWidth);
+
+	// Axis labels
 	xAxis
 		.selectAll()
 		.data(xTicks)
 		.join("text")
+		.classed("x-axis-label", true)
 		.text((date: Date) => {
 			const dayOfMonth = date.getDate();
 			if (dayOfMonth % 7 == 1 && dayOfMonth < 28) {
@@ -164,8 +186,51 @@ export function initializeLineGraph(
 		.attr(
 			"transform",
 			(d: Date) =>
-				`translate(${xScale(d) + 7},${
-					yScale(minVal) + tickLength + 7
+				`translate(${xScale(d) + plotAesthetics.axis.style.labelTranslateX},${
+					yScale(minVal) +
+					tickLength +
+					plotAesthetics.axis.style.labelTranslateY
 				}) rotate(-60)`,
 		);
+	yAxis
+		.selectAll()
+		.data(yTicks)
+		.join("text")
+		.classed("y-axis-label", true)
+		.text((y: number) => {
+			const firstDigit = +y.toString()[0];
+			if (firstDigit <= 4 || firstDigit === 6) {
+				return yFormatter(y);
+			}
+			return "";
+		})
+		.attr("x", xScale(minDate) - plotAesthetics.axis.style.tickLength - 3)
+		.attr("y", yScale)
+		.attr("text-anchor", "end")
+		.attr("dominant-baseline", "middle")
+		.attr("font-size", "70%");
+
+	// Axes themselves
+	xAxis
+		.selectAll()
+		.data([minDate, maxDate])
+		.join("line")
+		.classed("x-axis-axis", true)
+		.attr("x1", xScale)
+		.attr("x2", xScale)
+		.attr("y1", yScale(minVal))
+		.attr("y2", yScale(maxVal))
+		.attr("stroke", axisColor)
+		.attr("stroke-width", strokeWidth);
+	yAxis
+		.selectAll()
+		.data([minVal, maxVal])
+		.join("line")
+		.classed("y-axis-axis", true)
+		.attr("x1", xScale(minDate))
+		.attr("x2", xScale(maxDate))
+		.attr("y1", yScale)
+		.attr("y2", yScale)
+		.attr("stroke", axisColor)
+		.attr("stroke-width", strokeWidth);
 }
