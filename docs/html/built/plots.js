0cb6b2ed67504dd98ba37aa1e8ec8bfbeaa6666f
Robert Bennett <rltbennett@icloud.com>
2020-06-29 15:17:20 -0400

    Auto update with new data

--- docs/html/built/plots.js
+++ docs/html/built/plots.js
@@ -34,7 +34,7 @@ function assignData(allCovidData, allGeoData) {
     });
 }
 Promise.all([
-    d3.json("./data/covid_data-0f2376cd94e7ee02e9e7966c48f99f5cd1c58d47.json"),
+    d3.json("./data/covid_data-2649c78ffc500586ae478a3fee133478ffbcf8cd.json"),
     d3.json("./data/geo_data-be6715bfac29cf1d59f8c05b805ce8db5b42283f.json"),
 ]).then(objects => {
     const allCovidData = objects[0];
