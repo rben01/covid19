349c2d69cb7b1b5d004b9ff2e78e3abd0f4b0b3d
Robert Bennett <rltbennett@icloud.com>
2020-07-17 22:17:07 -0400

    Auto update with new data

--- docs/html/built/plots.js
+++ docs/html/built/plots.js
@@ -34,7 +34,7 @@ function assignData(allCovidData, allGeoData) {
     });
 }
 Promise.all([
-    d3.json("./data/covid_data-e251a28250e5cc7ee3485324eb136eccebfb5849.json"),
+    d3.json("./data/covid_data-b7deea85d60ef8986ad52fe211e1f38a961b3264.json"),
     d3.json("./data/geo_data-be6715bfac29cf1d59f8c05b805ce8db5b42283f.json"),
 ]).then(objects => {
     const allCovidData = objects[0];
--- docs/html/plots.ts
+++ docs/html/plots.ts
@@ -55,7 +55,7 @@ function assignData(allCovidData: AllCovidData, allGeoData: AllGeoData) {
 
 // Use the custom digest of the data file to only pull from the web anew, ignoring browser cache, when data has actually updated
 Promise.all([
-	d3.json("./data/covid_data-e251a28250e5cc7ee3485324eb136eccebfb5849.json"),
+	d3.json("./data/covid_data-b7deea85d60ef8986ad52fe211e1f38a961b3264.json"),
 	d3.json("./data/geo_data-be6715bfac29cf1d59f8c05b805ce8db5b42283f.json"),
 ]).then(objects => {
 	const allCovidData: AllCovidData = objects[0];
