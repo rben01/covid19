989c2aba889c962183d17b39d90c7f785e597f82
Robert Bennett <rltbennett@icloud.com>
2020-09-17 09:03:07 -0400

    Auto update with new data

--- docs/html/plots.ts
+++ docs/html/plots.ts
@@ -55,7 +55,7 @@ function assignData(allCovidData: AllCovidData, allGeoData: AllGeoData) {
 
 // Use the custom digest of the data file to only pull from the web anew, ignoring browser cache, when data has actually updated
 Promise.all([
-	d3.json("./data/covid_data-5714a938e71f6ef033ec1b80152bec8c38100814.json"),
+	d3.json("./data/covid_data-6280c4239ada3487ec2ad50b7bbb2425000e55bf.json"),
 	d3.json("./data/geo_data-be6715bfac29cf1d59f8c05b805ce8db5b42283f.json"),
 ]).then(objects => {
 	const allCovidData: AllCovidData = objects[0];
